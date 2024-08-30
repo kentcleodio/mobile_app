f6a3c98d15a3780f527f8f2f0bf446d85f3fc3f2
Kent Cleo Dio <kentcleodio@gmail.com>
2024-08-31 03:06:05 +0800

    Commit
    31/08/2024

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -9,6 +9,7 @@ import 'package:mobile_app/services/firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
